a13a478d8dc7f13e998f9ca26dfc698fce85a2f2
khalifa3isa <119413684+khalifa3isa@users.noreply.github.com>
2024-11-25 23:31:57 +0400

    Delete src/scheduling directory

--- src/scheduling/Scheduler.java
+++
@@ -1,8 +0,0 @@
-// Scheduler.java
-// Handles task scheduling for aides and clients.
-public class Scheduler {
-    public void scheduleTask(Task task, String aide, String client) {
-        // TODO: Implement scheduling logic here.
-        System.out.println("Scheduling task: " + task.getType());
-    }
-}
