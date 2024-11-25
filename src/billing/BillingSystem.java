40c01894789300c1e795070dd920d1d7ae320d4e
khalifa3isa <119413684+khalifa3isa@users.noreply.github.com>
2024-11-25 23:28:51 +0400

    Delete src/billing directory

--- src/billing/BillingSystem.java
+++
@@ -1,8 +0,0 @@
-// BillingSystem.java
-// Generates monthly invoices based on completed tasks.
-public class BillingSystem {
-    public void generateInvoice(String clientName, Task[] tasks) {
-        // TODO: Implement billing logic here.
-        System.out.println("Generating invoice for " + clientName);
-    }
-}
